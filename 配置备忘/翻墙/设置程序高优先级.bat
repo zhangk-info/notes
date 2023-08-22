5d25eb3c49df4559f7d727ffab2ff0da5e58f785
zhangkun <13628344382@139.com>
2023-08-22 17:23:45 +0800

    .

--- 配置备忘/翻墙/设置程序高优先级.bat
+++ 配置备忘/翻墙/设置程序高优先级.bat
@@ -1,1 +1,26 @@
+@echo off
+:: %1 -- The first Parameter
+
+:start
+tasklist|findstr -i "v2rayN.exe"
+if ERRORLEVEL 1 (goto err) else (goto continue)
+ 
+:err
+echo v2rayN.exe not ok.
+@REM 延时5s
+choice /t 5 /d y /n >nul
+goto :start
+ 
+:continue
+echo .
+echo .
+echo .
+echo .
 wmic process where "name='v2rayN.exe'" call setpriority "Realtime"
+echo .
+echo .
+echo .
+echo .
+echo v2rayN.exe is ok, start install service.
+choice /t 5 /d y /n >nul
+@REM
